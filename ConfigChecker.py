84243a865e105b8dc95f24d80fce51e9d1a56129
MichaelBoehmcke <MichaelBoehmcke@users.noreply.github.com>
2018-08-01 12:39:45 -0400

    Config Checks, No user input

--- ConfigChecker.py
+++ ConfigChecker.py
@@ -1,24 +1,20 @@
-from pathlib import Path
-import json
 import glob, os
 
 try:
     file = open ("config.conf", "r")
+    keys = file.readlines()
+    keys = [keys[0][12:].split("\n")[0], keys[1][12:]]
+    print(keys)
 
 except IOError as er:
-    print("Missing file path:\n" + "config.conf\n")
+    print("Missing file path 'config.conf' or missing values")
     try:
         keys = [os.environ["WATSON_USER"], os.environ["WATSON_PASS"]]
         print(keys)
     except KeyError as er:
-        print(
-            "Missing environment variables:\nWATSON_USER\nWATSON_PASS\n")
-        RunRequest = input ("Please run a set-up command to set your Watson Username and Password."
-                               "\nUse one of the following"
-                               "\ntemp setup config\ntemp setup envar\n")
-
-        if RunRequest == "temp setup config":
-            file = open("config.conf", "w")
-            file.write("watson_user=\nwatson_pass=")
-        else:
-            print("Exiting Program...")
+        print("Missing environment variables:\nWATSON_USER\nWATSON_PASS\n")
+        print("Neither Config file nor Environmental Variables exist. Generating config.conf...")
+        file = open("config.conf", "w")
+        file.write("watson_user=\nwatson_pass=\n")
+        print("Please access the config or environmental variables and set the values for Watson Username and Pssword. This program will now exit.")
+        print("Exiting Program...")
