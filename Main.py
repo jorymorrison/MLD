8827d61017b9affbcebe8bd98d7da49e3a7aaf53
Dylan Page <dylan.page@cox.net>
2018-08-15 12:28:29 -0400

    Proto Event Stream
    Not fully featured and will not be until integration, but this is a general feel of what the final Event Stream will look like.

--- Main.py
+++ Main.py
@@ -27,7 +27,7 @@ def tfidf(word, blob, bloblist):
 try:
     keys = [os.environ["WATSON_USER"], os.environ["WATSON_PASS"]]
 except KeyError as er:
-    print("Missing evironment variables:\nWATSON_USER\nWATSON_PASS\n\nSet them using values of your Watson API username and password.")
+    print("Failed to retrieve evironment variables:\nWATSON_USER\nWATSON_PASS\n\nSet them using values of your Watson API username and password.")
     exit()
 
 
@@ -45,21 +45,22 @@ while filefound == False:
         doc = fileContents(filename)
     except IOError as er:
         filefound = False
-        print(er.strerror + ". Check for correct spelling and/or location.")
+        print('Failed to retrieve file' + er.strerror)
 
-print('{\n\t"document_sentiment": {')
-print('\t\t"polarity": {},'.format(TextBlob(doc).polarity))
-print('\t\t"subjectivity": {}'.format(TextBlob(doc).subjectivity))
-print('\t},')
+logOutput = '{\n\t"document_sentiment": {'
+logOutput += '\n\t\t"polarity": {},'.format(TextBlob(doc).polarity)
+logOutput += '\n\t\t"subjectivity": {}'.format(TextBlob(doc).subjectivity)
+logOutput += '\n\t},\n'
+print("Successfully retrieved document sentiment...")
 
 
 try:
     content_type = 'application/json'
-    print(json.dumps(tone_analyzer.tone({"text": doc}, content_type, False), indent=4)[1:-2] + ",\n")
+    logOutput += json.dumps(tone_analyzer.tone({"text": doc}, content_type, False), indent=4)[1:-2] + ",\n"
 except WatsonApiException as er:
-    print("Method failed with status code " + str(er.code) + ": " + er.message)
+    print("Failed to retrieve document tone.\n Status code " + str(er.code) + ": " + er.message)
     exit()
-
+print("Successfully retrieved document tone...")
 
 #LexSig#
 
@@ -67,25 +68,32 @@ corpus = []
 
 invaldocs = 0
 
-os.chdir("corpus")
-for file in glob.glob("*.txt"):
-    try:
-        temp = open(file, "r")
-        corpus.append(TextBlob(temp.read()))
-    except UnicodeDecodeError as er:
-        invaldocs += 1
-#if invaldocs > 0:
-    #print("{} invalid documents in corpus.".format(invaldocs))
+try:
+    os.chdir("corpus")
+    for file in glob.glob("*.txt"):
+        try:
+            temp = open(file, "r")
+            corpus.append(TextBlob(temp.read()))
+        except UnicodeDecodeError as er:
+            invaldocs += 1
+except IOError as er:
+    print("Failed to access corpus.")
+    exit()
+
+print("Successfully accessed corpus with {} invalid documents...".format(invaldocs))
 
 doc = TextBlob(doc)
-print('\t"lexical_signature": {\n\t\t"tf-idf": [\n\t\t\t{')
+logOutput += '\n\t"lexical_signature": {\n\t\t"tf-idf": [\n\t\t\t{'
 scores = {word: tfidf(word, doc, corpus) for word in doc.words}
 sorted_words = sorted(scores.items(), key=lambda x: x[1], reverse=True)
 comma = 0
 for word, score in sorted_words[:5]:
     comma+=1;
-    out = '\t\t\t\t"{}": {}'
+    out = '\n\t\t\t\t"{}": {}'
     if comma < 5:
         out += ","
-    print(out.format(word, round(score, 5)))
-print("\t\t\t}\n\t\t]\n\t}\n}")
+    logOutput += out.format(word, round(score, 5))
+logOutput += "\n\t\t\t}\n\t\t]\n\t}\n}"
+print("Successfully calculated lexical signature...")
+
+#print(logOutput)
