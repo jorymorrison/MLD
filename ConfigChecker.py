9e9ebc2b71ebd086496bbc54416b2f16535ca9af
MichaelBoehmcke <MichaelBoehmcke@users.noreply.github.com>
2018-08-24 13:32:06 -0400

    Final Push
    Final push of config checker to Git in single program

--- ConfigChecker.py
+++ ConfigChecker.py
@@ -4,24 +4,32 @@ try:
     file = open ("config.conf", "r")
     keys = file.readlines()
     keys = [keys[0][12:].split("\n")[0], keys[1][12:]]
-    print(keys)
+    print("Successfully found Configuration file 'config.conf'")
+
     if keys[0] == "" or keys[1] == "":
-        print ("Config file missing username or password, please set these values.\n Trying environmental variables...")
+        print("Failed to read variables in config, missing values")
         try:
             keys = [os.environ["WATSON_USER"], os.environ["WATSON_PASS"]]
-            print(keys)
+            print("Successfully found Environmental Variables")
+            if keys[0] == "" or keys[1] == "":
+                print("Failed to find value for Environmental Variables")
+
         except KeyError as er:
-            print("Missing environment variables:\nWATSON_USER\nWATSON_PASS\nNeither config file or environmental variables are set, please access 'config.conf' or the environment and set these values.\nExiting Program...")
+            print("Failed to find Environmental Variables")
+            print("Please access the config or environmental variables and set the values for Watson Username and Pssword. This program will now exit.")
+            print("Exiting Program...")
 
 except IOError as er:
-    print("Missing file path 'config.conf'")
+    print("Failed to find file path for 'config.conf'")
     try:
         keys = [os.environ["WATSON_USER"], os.environ["WATSON_PASS"]]
-        print(keys)
+        print("Successfully found Environmental Variables")
+        if keys[0] == "" or keys[1] == "":
+            print("Failed to find value for Environmental Variables")
     except KeyError as er:
-        print("Missing environment variables:\nWATSON_USER\nWATSON_PASS\n")
-        print("Neither Config file nor Environmental Variables exist. Generating config.conf...")
+        print("Failed to find Environmental Variables")
         file = open("config.conf", "w")
         file.write("watson_user=\nwatson_pass=")
+        print("Successfully generated Configurtion file 'config.conf'")
         print("Please access the config or environmental variables and set the values for Watson Username and Pssword. This program will now exit.")
         print("Exiting Program...")
