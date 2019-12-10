b90bea8f4c5bbf3d70253c4e7980f6cf180b6f44
Matthew-Zorumski <matthew.zorumski@nhrec.org>
2019-12-09 19:35:12 -0500

    improved and optimized for calling function and resolved bug of overflow from previous runnings and previous curl calls.

---
+++ call2.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+depth=2
+input="/home/matthew/MLD/urls.txt"
+regex='*\/[0-9]{4}\/[0-9]{2}\/[0-9]{2}\/[a-z/-]+.html'
+echo 'https://www.cnn.com/' > urls.txt
+echo "filler" > scan.txt
+echo "" > temp.txt
+COUNTER=0
+while [  $COUNTER -lt $depth ]; do
+	echo $COUNTER
+	while IFS= read -r line
+	do
+		if grep -Fxq "$line" scan.txt
+		then
+			continue
+		else
+			echo "$line" >> scan.txt
+			echo "$line" | xargs -n1 curl | grep -Eo '*\/[0-9]{4}\/[0-9]{2}\/[0-9]{2}\/[a-z/-]+.html' | awk '$0="https://www.cnn.com"$0'>>temp.txt
+		fi
+	done < $input
+	while IFS= read -r line
+	do
+		echo "$line" >>urls.txt
+	done < temp.txt
+	echo "" > temp.txt
+	let COUNTER=COUNTER+1
+done
+sort urls.txt | uniq -u > sorted.txt
+
+while read a
+do
+	echo $a
+	python3 Main.py $a
+done < sorted.txt
+
+
+
+
+
+
+
