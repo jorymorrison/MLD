d588397379255e2f1a3d7dee428dcac229ade9b4
Matthew Zorumski <matt@Mitres-Mac-mini.local>
2019-12-12 17:50:47 -0500

    changed stuff

--- summon.sh
+++ summon.sh
@@ -11,12 +11,18 @@ while [  $COUNTER -lt $depth ]; do
 	echo $COUNTER
 	while IFS= read -r line
 	do
+#		echo "test"
 		if grep -Fxq "$line" scan.txt
 		then
 			continue
-		else
+#			echo "1"
+		else	
+#			echo "2"
 			echo "$line" >> scan.txt
-			echo "$line" | xargs -n1 curl | grep -Eo '*\/[0-9]{4}\/[0-9]{2}\/[0-9]{2}\/[a-z/-]+.html' | awk '$0="https://www.cnn.com"$0'>>temp.txt
+#			echo "$line"
+#			echo "\n"
+			echo "$line" | xargs -n1 curl -kL | grep -Eo '\/[0-9]{4}\/[0-9]{2}\/[0-9]{2}\/[a-z\/-]+\.html' | awk '$0="https://www.cnn.com"$0' >> temp.txt
+#			echo "done"
 		fi
 	done < $input
 	while IFS= read -r line
@@ -24,19 +30,12 @@ while [  $COUNTER -lt $depth ]; do
 		echo "$line" >>urls.txt
 	done < temp.txt
 	echo "" > temp.txt
-	let COUNTER=COUNTER+1
+	COUNTER=$(($COUNTER+1))
 done
 sort urls.txt | uniq -u > sorted.txt
 
-while read a
-do
-	echo $a
-	python3 Main.py $a
-done < sorted.txt
-
-
-
-
-
-
-
+#while read a
+#do
+#	echo $a
+#	python3 MLD.py $a
+#done < sorted.txt
