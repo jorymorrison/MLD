dfb92dde2cd140eb86dedfc2645299936fdc6c47
Matthew Zorumski <matt@Mitres-Mac-mini.local>
2019-12-12 16:37:26 -0500

    added code from mac function

--- MLD.py
+++ MLD.py
@@ -5,6 +5,14 @@ from goose3 import Goose
 from goose3.configuration import Configuration
 from watson_developer_cloud import ToneAnalyzerV3, WatsonApiException
 import nltk
+import ssl	
+
+try:	
+    _create_unverified_https_context = ssl._create_unverified_context	
+except AttributeError:	
+    pass	
+else:	
+    ssl._create_default_https_context = _create_unverified_https_context	
 
 nltk.download('punkt')
 
