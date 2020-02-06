cef5f4db527cae9c89b250fcfa5264eada39c2e5
Matthew-Zorumski <56372593+Matthew-Zorumski@users.noreply.github.com>
2020-02-06 15:52:35 -0500

    Add files via upload
    *Warning reading output from file will cause lag*
    created reader file that performs chi squared test on matching tones still working on limiting for  matching lexical signature.

--- MLD/outputs/reader.py
+++ MLD/outputs/reader.py
@@ -1,9 +1,23 @@
-import os
 import json
 import glob
+import numpy
+from scipy.stats import chisquare
 contents=[]
+outputs=[]
 for file in glob.glob('*.json'):
 	files = open(file, 'r')
 	data=json.load(files)
 	contents.append(data)
-print(contents)
+for w in range(len(contents)):
+    for x in range(len(contents)):
+        if x==w:
+            continue
+        elif contents[x]['article']['title']==contents[w]['article']['title']:
+            continue
+        else:
+            test=[]
+            for y in range(len(contents[w]['tone'])):
+                for z in range(len(contents[x]['tone'])):
+                        if contents[w]['tone'][y]['tone_id']==contents[x]['tone'][z]['tone_id']:
+                                outputs.append(chisquare([contents[w]['tone'][y]['score'], contents[x]['tone'][z]['score']]))
+print(outputs)
