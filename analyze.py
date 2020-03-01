eac969fc5cd8308ce080e12682851ec703846b4e
Matthew <matthew.zorumski@gmail.com>
2020-03-01 11:27:19 -0800

    removed empty files and improved analyze.py

--- analyze.py
+++ analyze.py
@@ -1,19 +1,15 @@
 import os
 import json
 import glob
-contents=[]
-for file in glob.glob('*.json'):
-	files = open(file, 'r')
-	data=json.load(files)
-	contents.append(data)
-print(contents)
-import json
-import glob
+import datetime
 import numpy
 from scipy.stats import chisquare
+date = str(datetime.datetime.now())
+path = os.path.split(os.path.abspath(__file__))[0] + '/comparison-' + date + ".json"
+results = open(path, 'a+')
 contents=[]
 outputs=[]
-for file in glob.glob('*.json'):
+for file in glob.glob('outputs/*.json'):
 	files = open(file, 'r')
 	data=json.load(files)
 	contents.append(data)
@@ -26,15 +22,17 @@ for w in range(len(contents)):
         else:
             test=[]
             count=0
-            for c in list(contents[w][signature].keys()):
-                for d in list(contents[x][signature].keys()):
+            for c in list(contents[w]['signature'].keys()):
+                for d in list(contents[x]['signature'].keys()):
                     if c==d:
-                        count++
+                        count+=1
                     else:
                         continue
                     if count>=3:
                         for y in range(len(contents[w]['tone'])):
                             for z in range(len(contents[x]['tone'])):
                                     if contents[w]['tone'][y]['tone_id']==contents[x]['tone'][z]['tone_id']:
-                                            outputs.append(chisquare([contents[w]['tone'][y]['score'], contents[x]['tone'][z]['score']]))
-print(outputs)
+                                            outputs.append({'comparison' : [contents[w]['article']['title'],contents[x]['article']['title']],'results' : chisquare([contents[w]['tone'][y]['score'], contents[x]['tone'][z]['score']])})
+
+output=json.dumps(outputs, indent=4, sort_keys=True)
+results.write(output)
